623df386b379376df1fe87e242911e757867f5ea
Hayat ID bihi <93680879+Ha-yatid@users.noreply.github.com>
2024-07-22 21:21:40 +0100

    Add files via upload

---
+++ modules/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);

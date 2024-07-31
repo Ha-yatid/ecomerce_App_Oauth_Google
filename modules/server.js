0beca3391f6268a64b834b1da9987b1ce7d2d6f3
Hayat ID bihi <93680879+Ha-yatid@users.noreply.github.com>
2024-07-31 23:53:41 +0100

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -1,11 +1,3 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB connected...'))
-//     .catch(err => console.error('MongoDB connection error:', err));
-
 const mongoose = require('mongoose');
 const app = require('./index');
 require('dotenv').config();

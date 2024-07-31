57dd898019d1054a33defe19a6f1c6a95ff190df
Hayat ID bihi <93680879+Ha-yatid@users.noreply.github.com>
2024-07-31 23:54:53 +0100

    Update User.js

--- modules/models/User.js
+++ modules/models/User.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true, enum: ['admin', 'user'], default: 'user' }
+    role: { type: String, required: true, enum: ['admin', 'user'], default: 'user' },
+    isEmailVerified: { type: Boolean, default: false },
+    emailVerificationCode: { type: String },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
 });
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
